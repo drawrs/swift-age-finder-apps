4fb8dd4542ff8fb62f90e5f59bd6108490933dc6
Rizal Hilman <rizal.drawrs@gmail.com>
2019-08-06 09:58:37 +0700

    Initial Commit

---
+++ AgeFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AgeFinder
+//
+//  Created by Rizal Hilman on 06/08/19.
+//  Copyright © 2019 Rizal Hilman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
